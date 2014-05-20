56401c8e8309ca457bf3d00d29a7cb530f36ecdf
rojena <rojenaahmed4@hotmail.com>
2014-05-19 17:56:58 -0700

    First Commit

---
+++ Test_On_Pharmacy_Management_System/Test_On_Logic_Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WPF_SWE2_Phase1;
+
+namespace Test_On_Pharmacy_Management_System
+{
+    [TestFixture, RequiresSTA]
+    class Test_On_Logic_Functions
+    {
+        public  UserLogic user_test=new UserLogic();
+        
+        [Test]
+        public void test_on_userLogin_parameters()
+        {
+            bool actual = user_test.Login_Logic("Zabady", "123");
+            Assert.IsTrue(actual);
+        }
+    }
+}
